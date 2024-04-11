aee2c63553c92d6b10d2de60767d3af6ceda898c
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-04-11 20:26:32 +0200

    premier ajout du composant bouttons

---
+++ client/src/components/Buttons.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import "./Buttons.css";
+
+function Buttons({ buttonIndex }) {
+  const buttonsArray = [
+    { id: 0, src: "./public/assets/images/icons/cart4.svg", name: "cart" },
+    { id: 1, src: "./public/assets/images/icons/heart4.svg", name: "fav" },
+    {
+      id: 2,
+      src: "./public/assets/images/icons/add-cart.svg",
+      name: "cartadd",
+    },
+    {
+      id: 3,
+      src: "./public/assets/images/icons/add-heart.svg",
+      name: "heartadd",
+    },
+  ];
+
+  /* Fonction pour ajouter au "tableau" panier */
+
+  /* Fonction pour ajouter au "tableau" favoris */
+
+  return (
+    <div>
+      <button type="button">
+        <img
+          src={buttonsArray[buttonIndex].src}
+          alt={buttonsArray[buttonIndex].src}
+        />
+      </button>
+    </div>
+  );
+}
+
+Buttons.propTypes = {
+  buttonIndex: PropTypes.number.isRequired,
+};
+
+export default Buttons;
