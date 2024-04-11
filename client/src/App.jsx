6d1cdc1b1ad36fc2ef71c7c297a6ef2f068b516b
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-04-11 17:20:53 +0200

    modif

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,7 @@ import "./App.css";
 import Delivery from "./components/Delivery";
 
 function App() {
-  return (
-    
-   <Delivery/>
-   
-   
-  );
+  return <Delivery />;
 }
 
 export default App;
