25a9cd5e30c37194f8e1151bf543925816d22c79
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-04-13 17:10:32 +0200

    design page panier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import CartContainer from "./components/CartContainer";
 
 function App() {
-  return (<>
-    <h1>Feet & fun</h1>
+  return (
+
     <CartContainer/>
-</>
+
   );
 }
 
