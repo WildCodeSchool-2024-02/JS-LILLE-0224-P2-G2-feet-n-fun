b75a8eae762d1f611e709c6d7b1a206c6c424074
EMILIE LINGAT <emilie.lingat@gmail.com>
2024-04-16 09:26:55 +0200

    deleting blank lines

--- client/src/components/Delivery.jsx
+++ client/src/components/Delivery.jsx
@@ -30,108 +30,106 @@ function Delivery() {
 
   return (
     <>
-
-       <button type="button" className="buttonCloseDeliveryPayment">
-        <img src="./public/assets/images/icons/exit-btn-red.svg" alt="croix"/>
+      <button type="button" className="buttonCloseDeliveryPayment">
+        <img src="./public/assets/images/icons/exit-btn-red.svg" alt="croix" />
       </button>
-
       <h2>Livraison</h2>
-      <p className="deliveryDate">Délais de livraison :   3 à 5 jours ouvrés</p>
-
+      <p className="deliveryDate">Délais de livraison : 3 à 5 jours ouvrés</p>
       <section className="deliverySection">
         <form className="deliveryForm">
           <h3>TOTAL : €</h3>
-          <div className='deliveryFormContainer'>
-            <div ><h3>Contact</h3>
-
-            <label>
-              Prénom{" "}
-              <div>
-                <input
-                  type="text"
-                  name="name"
-                  onChange={changeClientName}
-                  required
-                />
-              </div>
-            </label>
-            <label>
-              Nom{" "}
-              <div>
-                <input
-                  type="text"
-                  name="surname"
-                  onChange={changeClientSurname}
-                  required
-                />
-              </div>
-            </label>
-            <label>
-              Adresse e-mail
-              <div>
-                <input
-                  type="email"
-                  name="email"
-                  onChange={changeClientMail}
-                  required
-                />
-              </div>
-            </label>
-            <label>
-              Numéro de téléphone{" "}
-              <div>
-                <input type="tel" name="phone" required />
-              </div>
-            </label>
-            <br /> </div>
+          <div className="deliveryFormContainer">
             <div>
-            <h3>Adresse de livraison</h3>
-            <label>
-              N°{" "}
-              <div>
-                <input
-                  className="numberInput"
-                  type="number"
-                  name="numberRoad"
-                  required
-                />
-              </div>
-            </label>
-            <label>
-              {" "}
-              Voie{" "}
-              <div>
-                <input type="text" name="street" required />
-              </div>
-            </label>
-            <label>
-              Code Postal
-              <div>
-                <input type="number" name="postalcode" required />
-              </div>
-            </label>
-            <label>
-              Ville{" "}
-              <div>
-                <input type="text" name="postalcode" required />
-              </div>
-            </label>
-            <label>
-              Pays{" "}
-              <div>
-                <input type="text" name="postalcode" required />
-              </div>
-            </label>
+              <h3>Contact</h3>
+              <label>
+                Prénom
+                <div>
+                  <input
+                    type="text"
+                    name="name"
+                    onChange={changeClientName}
+                    required
+                  />
+                </div>
+              </label>
+              <label>
+                Nom
+                <div>
+                  <input
+                    type="text"
+                    name="surname"
+                    onChange={changeClientSurname}
+                    required
+                  />
+                </div>
+              </label>
+              <label>
+                Adresse e-mail
+                <div>
+                  <input
+                    type="email"
+                    name="email"
+                    onChange={changeClientMail}
+                    required
+                  />
+                </div>
+              </label>
+              <label>
+                Numéro de téléphone
+                <div>
+                  <input type="tel" name="phone" required />
+                </div>
+              </label>
+              <br />
+            </div>
+            <div>
+              <h3>Adresse de livraison</h3>
+              <label>
+                N°
+                <div>
+                  <input
+                    className="numberInput"
+                    type="number"
+                    name="numberRoad"
+                    required
+                  />
+                </div>
+              </label>
+              <label>
+                Voie
+                <div>
+                  <input type="text" name="street" required />
+                </div>
+              </label>
+              <label>
+                Code Postal
+                <div>
+                  <input type="number" name="postalcode" required />
+                </div>
+              </label>
+              <label>
+                Ville
+                <div>
+                  <input type="text" name="postalcode" required />
+                </div>
+              </label>
+              <label>
+                Pays
+                <div>
+                  <input type="text" name="postalcode" required />
+                </div>
+              </label>
             </div>
           </div>
           <button
             onClick={displayPayment}
-            className="deliveryButton" type="submit">
-            Passer au paiement </button>
-        
+            className="deliveryButton"
+            type="submit"
+          >
+            Passer au paiement
+          </button>
         </form>
       </section>
-
       {showPayment && (
         <Payment
           clientName={clientName}
@@ -142,7 +140,5 @@ function Delivery() {
     </>
   );
 }
-
-
 
 export default Delivery;
