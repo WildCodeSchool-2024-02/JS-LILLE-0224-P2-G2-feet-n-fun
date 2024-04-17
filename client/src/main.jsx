27ebb6d90318017889f8e38216005783e8379b1e
Fantine <fantine.rudent@hotmail.fr>
2024-04-17 14:49:32 +0200

    Fantine Help loader

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
 /* import Home from "./pages/Home"; */
 import Favoris from "./pages/Favoris";
@@ -12,18 +10,14 @@ import Home from "./pages/Home";
 
 // Le useEffect fait un appel d'API au démarrage de la page et stock le resultat dans le state "data"
 
-const getData = () => {
-  let data;
-  axios
-    .get("https://fantinerudent.github.io/api-feet-n-fun/data.json")
-    .then((response) => {
-      data = response.data;
-    })
-    .then(() => {
-    });
+const getData = async () => {
+  const result = await fetch(
+    "https://fantinerudent.github.io/api-feet-n-fun/data.json"
+  );
+  const datas = await result.json();
+  return datas;
 };
 
-console.log("data", getData());
 const router = createBrowserRouter([
   {
     element: <App />,
@@ -31,12 +25,16 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-        loader: () =>{ getData()}
-        },
+        loader: () => getData(),
+      },
 
       {
         path: "/categories/:id",
         element: <Categories />,
+        loader: async ({ params }) => {
+          const datas = await getData();
+          return datas[parseInt(params.id, 10) - 1].products;
+        },
       },
       { path: "/favoris", element: <Favoris /> },
       { path: "/panier", element: <Panier /> },
@@ -47,7 +45,3 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
-
-// loader: ({ params }) => {
-//   getData().filter((item) => parseInt(params) === item.id);
-// },
