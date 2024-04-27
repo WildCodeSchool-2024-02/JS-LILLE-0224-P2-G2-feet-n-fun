b105bf9f29e8e7a67666793ad5d30f03b033777c
iSayZ <estrine.alexis@gmail.com>
2024-04-27 16:37:22 +0200

    fixing alert cart and fav

--- client/src/components/Header/InformationBar/InformationBar.jsx
+++ client/src/components/Header/InformationBar/InformationBar.jsx
@@ -1,12 +1,27 @@
-import './InformationBar.css';
+import "./InformationBar.css";
+import { useContext } from "react";
+import { ShopContext } from "../../../context/ShopContext";
+import Popup from "../../Other/Popup/Popup";
 
 function InformationBar() {
-    return (
-        <div className='information-bar'>
-                <p className='marquee one'>Livraison offerte à partir de 25€ d'achat</p>
-                <p className='marquee two'>20% de reduction avec le code promo WCS2024</p>
-        </div>
-    )
+  const { showAlertCart, showAlertFav } = useContext(ShopContext);
+
+  return (
+    <>
+      <div className="information-bar">
+        <p className="marquee one">Livraison offerte à partir de 25€ d'achat</p>
+        <p className="marquee two">
+          20% de reduction avec le code promo WCS2024
+        </p>
+      </div>
+      {showAlertCart && (
+        <Popup content="Votre article a bien été ajouter au panier !" />
+      )}
+      {showAlertFav&& (
+        <Popup content="Votre article a bien été ajouter aux favoris !" />
+      )}
+    </>
+  );
 }
 
 export default InformationBar;
--- client/src/components/Other/Popup/Popup.jsx
+++ client/src/components/Other/Popup/Popup.jsx
@@ -1,11 +1,16 @@
 import './Popup.css'
 import PropTypes from "prop-types";
+import { useContext } from 'react';
+import { ShopContext } from '../../../context/ShopContext';
 
 // Pour utiliser le popup, initialiser un state à false, mettre dans la fonction d'ajout au panier un changement du state à false
 // en début de fonction et un passage à true en fin de fonction. 
 function Popup ({ content }) {
+
+    const { showAlertFav } = useContext(ShopContext) 
+
     return (
-        <div className="notification">
+        <div className="notification" id={showAlertFav && "fav"}>
             <div className="notification__body">
                 <img
                     src="/public/assets/images/icons/check-circle.svg"
@@ -14,7 +19,7 @@ function Popup ({ content }) {
                 />
                 {content} &#128640; 
             </div>
-            <div className="notification__progress" />
+            <div className="notification__progress" id={showAlertFav && "fav"} />
         </div>
     )
 }
